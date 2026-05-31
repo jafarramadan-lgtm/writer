ff3de2c934f7196358faf2ead6d7d16cc7ce3137
Jafar ramadan <ramadangafer5@gmail.com>
2026-05-31 20:00:57 +0300

    Update middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -4,6 +4,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   console.log(token)
+  console.log("jjjjjj")
   const isProtectedRoute =
     request.nextUrl.pathname.startsWith("/writer") ||
     request.nextUrl.pathname.startsWith("/reader");
