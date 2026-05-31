b4e509fa031ea891780c23aeaf912ca21d40e968
Jafar ramadan <ramadangafer5@gmail.com>
2026-05-31 17:56:28 +0300

    Update middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -23,7 +23,7 @@ export async function middleware(request: NextRequest) {
       const response = await fetch("https://back-writer.onrender.com/roleCheck", {
         method: "GET",
         headers: {
-          authorization: Bearer ${token},
+          authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
         credentials: "include",
